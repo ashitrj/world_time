d73e04e402d76ebde62db5e3ce62d15961cdd156
ashitr <ashitr@iitbhilai.ac.in>
2022-01-14 22:06:49 +0530

    Initial commit

---
+++ lib/pages/loading.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  @override
+  _LoadingState createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(url: 'Indian/Maldives', location: 'India', flag: 'india.png');
+    await instance.getTime();
+    Navigator.pushReplacementNamed(context, '/home', arguments: {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime,
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitWave(
+          color: Colors.blue,
+          size: 50.0,
+        )
+      ),
+    );
+  }
+}
